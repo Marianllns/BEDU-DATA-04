1b8b195a2643d6e07bfd0097b0346ec1b5fa74e7
Marianllns <mariana.alonllns@outlook.com>
2024-01-10 21:34:48 -0600

    Instalación de pandas

--- 01_github_followers.py
+++ 01_github_followers.py
@@ -10,18 +10,31 @@ def get_github_user(username):
         return response.json()
     return None
 
-def download_avatar_user(avatar_url):
+
+def download_avatar_user(avatar_url, username):
     response = requests.get(avatar_url)
     if response.status_code == 200:
-        #download image
-        respons_content = response.content
-        filename = 'avatar.png'
+        # Download image
+        response_content = response.content
+        filename = f'tmp/{username}.png'
         with open(filename, 'wb') as image:
-            image.write(respons_content)
+            image.write(response_content)
             return filename
     return None
 
+def get_user_folowers(username):
+    url = f'{BASE_URL}users/{username}/followers'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    return None
 
 username = input('Give me an username you want to extract the information:\n')
 selected_user = get_github_user(username)
-print(download_avatar_user(selected_user.get('avatar_url')))
+
+user_folowers = get_user_folowers(username)
+
+for follower in user_folowers:
+    folower_avatar_url = follower.get('avatar_url')
+    folower_username = follower.get('login')
+    download_avatar_user(folower_avatar_url, folower_username)
