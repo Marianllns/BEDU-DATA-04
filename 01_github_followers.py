396db6db389e025d2e1718c371385896ff0c2f4e
Marianllns <mariana.alonllns@outlook.com>
2024-01-10 20:35:10 -0600

    download image in a temporary folder

---
+++ 01_github_followers.py
@@ -0,0 +1,27 @@
+import requests
+
+BASE_URL = 'https://api.github.com/'
+
+def get_github_user(username):
+    url = f'{BASE_URL}users/{username}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+def download_avatar_user(avatar_url):
+    response = requests.get(avatar_url)
+    if response.status_code == 200:
+        #download image
+        respons_content = response.content
+        filename = 'avatar.png'
+        with open(filename, 'wb') as image:
+            image.write(respons_content)
+            return filename
+    return None
+
+
+username = input('Give me an username you want to extract the information:\n')
+selected_user = get_github_user(username)
+print(download_avatar_user(selected_user.get('avatar_url')))
